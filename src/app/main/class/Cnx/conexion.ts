591aa17566fe166636bff7538e76f350cd422e92
JairMartinez86 <jmartinezg86@gmail.com>
2021-12-03 21:06:24 -0600

    cambio

--- src/app/main/class/Cnx/conexion.ts
+++ src/app/main/class/Cnx/conexion.ts
@@ -1,14 +1,24 @@
 export class Conexion {
 
-    private IP : string = "jmartinezapi.azurewebsites.net";
-    private PORT : String =  "";
+    /*private IP : string = "localhost";
+    private PORT : String =  "44311";
 
     public TimeVerif: number = 600;
     public TimeClose: number = 30;
 
 
     Url() : string{
-        return "https://"+this.IP+this.PORT+"/api/"; 
+        return "https://"+this.IP+":"+this.PORT+"/api/"; 
+    }*/
+
+    private IP : string = "jmartinezapi.azurewebsites.net";
+    private PORT : String =  "44311";
+
+    public TimeVerif: number = 600;
+    public TimeClose: number = 30;
+
+    Url() : string{
+        return "https://"+this.IP+"/api/"; 
     }
 
 }
